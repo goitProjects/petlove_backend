8fcf043e6c9efc724b26be4ec4dadcb4bed414f2
Ivetta Dashkova <ivettafsd@gmail.com>
2024-01-02 16:45:58 +0200

    delete upload middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,6 +3,5 @@ const validateId = require('./validateId');
 const validateQuery = require('./validateQuery');
 const isValidId = require('./isValidId');
 const authenticate = require('./authenticate');
-const upload = require('./upload');
 
-module.exports = { validateBody, isValidId, authenticate, upload, validateId, validateQuery };
+module.exports = { validateBody, isValidId, authenticate, validateId, validateQuery };

664451068c478a82c585452e7e5604f0efb3f468
IvettaDashkova <ivettafsd@gmail.com>
2024-02-20 12:13:44 +0100

    add logic with add pet to swagger

--- controllers/users/currentUserFull.js
+++ controllers/users/currentUserFull.js
@@ -4,7 +4,7 @@ const currentUserFull = async (req, res) => {
   const { _id } = req.user;
   const foundUser = await User.findById(_id).populate('noticesViewed noticesFavorites').populate('pets', '-owner');
   const { password, ...user } = foundUser.toObject();
-  res.json({ user });
+  res.json(user);
 };
 
 module.exports = currentUserFull;

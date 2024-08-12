607707acbe95614ed6646260037be712e8d54f98
z9877969 <z9877969@ukr.net>
2024-08-12 10:40:06 +0300

    fixes query params handling for /notes endpoint

--- controllers/notices/getNotices.js
+++ controllers/notices/getNotices.js
@@ -1,42 +1,37 @@
 const { Notice } = require('../../models/notice');
 
 const getNotices = async (req, res) => {
-  const { keyword='', category='', species='', locationId='', byDate = true, byPrice, byPopularity, page = 1, limit = 6  } = req.query;
+  const { keyword = '', category = '', species = '', locationId = '', byDate = true, byPrice, byPopularity, page = 1, limit = 6 } = req.query;
   const query = {};
 
   if (keyword) {
-    query.$or = [
-      { species: { $regex: keyword, $options: 'i' } },
-      { title: { $regex: keyword, $options: 'i' } }
-    ];
+    query.$or = [{ species: { $regex: keyword, $options: 'i' } }, { title: { $regex: keyword, $options: 'i' } }];
   }
-    category && (query.category = category);
-    species && (query.species = species);
-    locationId && (query.location = locationId);
-
+  category && (query.category = category);
+  species && (query.species = species);
+  locationId && (query.location = locationId);
 
   let sortOptions = { createdAt: JSON.parse(byDate) ? 1 : -1 };
   if (byPrice !== undefined) {
-      sortOptions = { price: JSON.parse(byPrice) ? 1 : -1 };
-      query.category = 'sell'
+    sortOptions = { price: JSON.parse(byPrice) ? 1 : -1 };
+    if (category && category !== 'sell') {
+      query.category = category;
+    } else {
+      query.category = 'sell';
+    }
   }
   if (byPopularity !== undefined) {
     sortOptions = { popularity: JSON.parse(byPopularity) ? 1 : -1 };
   }
-    
-    const skip = (page - 1) * limit;
-    
+
+  const skip = (page - 1) * limit;
 
   const noticesByQuery = await Notice.aggregate([
     { $match: query },
     {
       $facet: {
         totalCount: [{ $count: 'count' }],
-        results: [
-          { ...(sortOptions && { $sort: sortOptions }) },
-          { $skip: Number(skip) },
-          { $limit: Number(limit) },
-        ],
+        results: [{ ...(sortOptions && { $sort: sortOptions }) }, { $skip: Number(skip) }, { $limit: Number(limit) }],
       },
     },
     {
@@ -48,15 +43,13 @@ const getNotices = async (req, res) => {
       },
     },
   ]);
-    
-  
-    res.json({
+
+  res.json({
     page: Number(page),
     perPage: Number(limit),
     totalPages: Math.ceil(noticesByQuery[0].totalCount / limit) || 1,
     results: noticesByQuery[0].results,
   });
- 
 };
 
 module.exports = getNotices;

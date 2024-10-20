810980ee1ed056e68b7db040a0dd055bff243163
z9877969 <z9877969@ukr.net>
2024-10-21 01:28:11 +0300

    fixes swager to correct data

--- controllers/cities/index.js
+++ controllers/cities/index.js
@@ -1,10 +1,8 @@
 const { CtrlWrapper } = require('../../helpers');
 const getCitiesByQuery = require('./getCitiesByQuery');
-const setCurCities = require('./setCurCities');
 const getPetsLocations = require('./getPetsLocations');
 
 module.exports = {
   getCitiesByQuery: CtrlWrapper(getCitiesByQuery),
-  setCurCities: CtrlWrapper(setCurCities),
   getPetsLocations: CtrlWrapper(getPetsLocations),
 };
